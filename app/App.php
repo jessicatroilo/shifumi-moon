c4d97795b0d0bacfa02a6e24d81dbe9f54c4ec57
jessicatroilo <jessica.troilo25@gmail.com>
2024-02-20 14:37:41 +0100

    progression sur le css

--- app/App.php
+++ app/App.php
@@ -13,14 +13,14 @@
     if (isset ($_POST['choices'])) 
     {    
         $choice_user = $_POST['choices'];
-        $choices = array ("Pierre","Papier","Ciseau");
+        $choices = array ("Cristal","Parchemin","Dague");
         $key = array_rand($choices);
         $choice_computer = $choices[$key];         
 
         echo "<p>choix du joueur : $choice_user</p>";
         echo "<p>choix de l'ordinateur : $choice_computer</p>";
 
-        if ($choice_user === "Pierre" && $choice_computer === "Ciseau" || $choice_user === "Papier" && $choice_computer === "Pierre" || $choice_user === "Ciseau" && $choice_computer === "Papier" )
+        if ($choice_user === "Cristal" && $choice_computer === "Dague" || $choice_user === "Parchemin" && $choice_computer === "Cristal" || $choice_user === "Dague" && $choice_computer === "Parchemin" )
         { 
             $result = "<p> Gagné !<p>";
         } 
--- index.php
+++ index.php
@@ -3,28 +3,55 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS -->
     <link type = "text/css" rel="stylesheet" href="app/css/style.css">
+
+    <!-- Sources et crédits -->
+    <a href="https://fr.freepik.com/vecteurs-libre/vecteur-fond-espace-fond-ecran-galaxie_18938344.htm#query=fond%20lune%20etoile%20illustration&position=5&from_view=search&track=ais&uuid=f5949c29-26c2-432a-bba3-25e5f6368521">Image de rawpixel.com</a> sur Freepik
+    
     <title>Shifumi Moon</title>
+    
 </head>
 <body>
     <div id="container">
+        
         <header>
             <h1>Shifumi Moon</h1>
         </header>
+
         <main>
             
-            <div>
-                <form  method="post" action="">
-                    <input type="radio" name ="choices" value="Pierre">Pierre</input>
-                    <input type="radio" name ="choices" value="Papier">Papier</input>
-                    <input type="radio" name ="choices" value="Ciseau">Ciseau</input>
-                    <input type="submit" value="Jouez"></input>
-                </form>
-            </div>
-            <div> <!--aura un hidden et apparaitra quand le joueur aura cliquez sur envoyer --->
-            <?php include __DIR__.'/app/App.php'?>
-            </div>
+            <section>
+                <div class="form__wrapper">
+                    <form  class ="form" method="post" action="">
+
+                        <label class="form__label" for="pierre">Cristal</label>
+                        <input class="form__input" type="radio" id="pierre" name ="choices" value="Cristal"></input>
+
+                        <label class="form__label" for="papier">Parchemin</label>
+                        <input class="form__input" type="radio" id="papier" name ="choices" value="Parchemin"></input>
+
+                        <label class="form__label" for="pierre">Dague</label>
+                        <input class="form__input" type="radio" id="ciseau" name ="choices" value="Dague"></input>
+
+                        <input class="form__submit"type="submit" value="Jouez"></input>
+                        
+                    </form>
+                </div>
+            </section>
+
+            <section>
+                <div> <!--aura un hidden et apparaitra quand le joueur aura cliquez sur envoyer --->
+                <?php include __DIR__.'/app/App.php'?>
+                </div>
+            </section>
+
         </main>
+    
+        <footer>
+            Règle du jeu - Copyright&#169;2024 - Created by Jessica Troilo
+        </footer>
     </div>
 </body>
 </html>
